c903dd5ab4b1fea16e02434cd2613b7fd5221a1d
jlmiller35 <jlmiller35@hotmail.com>
2020-02-28 14:20:13 -0800

    Using SpawnActor<>() to Spawn

--- BattleTank2/Source/BattleTank2/Tank.h
+++ BattleTank2/Source/BattleTank2/Tank.h
@@ -9,6 +9,7 @@
 //Forward declarations
 class UTankBarrel;
 class UTankAimingComponent;
+class AProjectile;
 
 UCLASS()
 class BATTLETANK2_API ATank : public APawn
@@ -40,6 +41,13 @@ private:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	UPROPERTY(EditAnywhere, Category = Firing)
+	UPROPERTY(EditAnywhere, Category = "Firing")
 	float LaunchSpeed = 4000;
+
+	UPROPERTY(EditAnywhere, Category = "Setup")
+	TSubclassOf<AProjectile> ProjectileBlueprint;
+
+	//local reference for spawning projectile
+	UTankBarrel* Barrel = nullptr;
+
 };
